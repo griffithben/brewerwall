ea59b46cef5c338eef73a0fb98c6305236929190
griffithben <griffith.ben@gmail.com>
2014-03-28 02:05:15 -0500

    hardened test cases for beer_styles api

--- app/routes/beer_styles.php
+++ app/routes/beer_styles.php
@@ -48,42 +48,62 @@ $app->post('/api/beerstyles', function () use ($app, $adapter){
     $select->where->expression("LOWER(category) = ?", strtolower($post->category));
 
   //OG Property
-  if(isset($post->og) && is_numeric($post->og))
+  if(isset($post->og) && is_numeric($post->og)){
     $select->where->greaterThanOrEqualTo('og_max', $post->og)->lessThanOrEqualTo('og_min', (float) $post->og);
+    $select->where("(og_max = 0 AND og_min = 0)", 'OR');
+  }
 
   //OG_Plato Property
-  if(isset($post->og_plato) && is_numeric($post->og_plato))
+  if(isset($post->og_plato) && is_numeric($post->og_plato)){
     $select->where->greaterThanOrEqualTo('og_plato_max', $post->og_plato)->lessThanOrEqualTo('og_plato_min', $post->og_plato);
+    $select->where("(og_plato_max = 0 AND og_plato_min = 0)", 'OR');
+  }
 
   //FG Property
-  if(isset($post->fg) && is_numeric($post->fg))
+  if(isset($post->fg) && is_numeric($post->fg)){
     $select->where->greaterThanOrEqualTo('fg_max', $post->fg)->lessThanOrEqualTo('fg_min', $post->fg);
+    $select->where("(fg_max = 0 AND fg_min = 0)", 'OR');
+  }
 
   //FG_Plato Property
-  if(isset($post->fg_plato) && is_numeric($post->fg_plato))
+  if(isset($post->fg_plato) && is_numeric($post->fg_plato)){
     $select->where->greaterThanOrEqualTo('fg_plato_max', $post->fg_plato)->lessThanOrEqualTo('fg_plato_min', $post->fg_plato);
+    $select->where("(fg_plato_max = 0 AND fg_plato_min = 0)", 'OR');
+  }
 
   //ABW Property
-  if(isset($post->abw) && is_numeric($post->abw))
+  if(isset($post->abw) && is_numeric($post->abw)){
     $select->where->greaterThanOrEqualTo('abw_max', $post->abw)->lessThanOrEqualTo('abw_min', $post->abw);
+    $select->where("(abw_max = 0 AND abw_min = 0)", 'OR');
+  }
 
   //ABV Property
-  if(isset($post->abv) && is_numeric($post->abv))
+  if(isset($post->abv) && is_numeric($post->abv)){
     $select->where->greaterThanOrEqualTo('abv_max', $post->abv)->lessThanOrEqualTo('abv_min', $post->abv);
+    $select->where("(abv_max = 0 AND abv_min = 0)", 'OR');
+  }
 
   //IBU Property
-  if(isset($post->ibu) && is_numeric($post->ibu))
+  if(isset($post->ibu) && is_numeric($post->ibu)){
     $select->where->greaterThanOrEqualTo('ibu_max', $post->ibu)->lessThanOrEqualTo('ibu_min', $post->ibu);
+    $select->where("(ibu_max = 0 AND ibu_min = 0)", 'OR');
+  }
 
   //SRM Property
-  if(isset($post->srm) && is_numeric($post->srm))
+  if(isset($post->srm) && is_numeric($post->srm)){
     $select->where->greaterThanOrEqualTo('srm_max', $post->srm)->lessThanOrEqualTo('srm_min', $post->srm);
+    $select->where("(srm_max = 0 AND srm_min = 0)", 'OR');
+    $select->where("(srm_min = srm_max AND srm_max != 0 AND srm_max <= ".$adapter->platform->quoteValue($post->srm).")", 'OR');
+  }
 
   //EBC Property
-  if(isset($post->abw) && is_numeric($post->abw))
-    $select->where->greaterThanOrEqualTo('abw_max', $post->abw)->lessThanOrEqualTo('abw_min', $post->abw);
+  if(isset($post->ebc) && is_numeric($post->ebc)){
+    $select->where->greaterThanOrEqualTo('ebc_max', $post->ebc)->lessThanOrEqualTo('ebc_min', $post->ebc);
+    $select->where("(ebc_max = 0 AND ebc_min = 0)", 'OR');
+    $select->where("(ebc_max = ebc_min AND ebc_max != 0 AND ebc_max <= ".$adapter->platform->quoteValue($post->ebc).")", 'OR');
+  }
 
-  //ABW Property
+  //Year Property
   if(isset($post->year) && is_numeric($post->year))
     $select->where(array("year"=>$post->year));
 
--- tests/integration/BeerStylesTest.php
+++ tests/integration/BeerStylesTest.php
@@ -21,11 +21,115 @@ class BeerStylesTest extends Slim_Framework_TestCase{
       $this->assertEquals(1, count($styles));
     }
 
-    public function testBeerStylePost(){
-      $parameters = array('og' => 1.080);
+    public function testBeerStylePostName(){
+      $parameters = array('name' => "english");
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(6, count($styles));
+    }
+
+    public function testBeerStylePostDescription(){
+      $parameters = array('description' => "english");
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(13, count($styles));
+    }
+
+    public function testBeerStylePostOrigin(){
+      $parameters = array('origin' => "British");
       $this->post('/api/beerstyles', $parameters);
       $styles = json_decode($this->response->body());
       $this->assertEquals(200, $this->response->status());
       $this->assertSame(21, count($styles));
+    }
+
+    public function testBeerStylePostCategory(){
+      $parameters = array('category' => "Ale");
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(76, count($styles));
+    }
+
+    public function testBeerStylePostOG(){
+      $parameters = array('og' => 1.080);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(32, count($styles));
+    }
+
+    public function testBeerStylePostOGPlato(){
+      $parameters = array('og_plato' => 12);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(74, count($styles));
+    }
+
+    public function testBeerStylePostFG(){
+      $parameters = array('fg' => 1.008);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(51, count($styles));
+    }
+
+    public function testBeerStylePostFGPlato(){
+      $parameters = array('fg_plato' => 2);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(79, count($styles));
+    }
+
+    public function testBeerStylePostABW(){
+      $parameters = array('abw' => 8);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(33, count($styles));
+    }
+
+    public function testBeerStylePostABV(){
+      $parameters = array('abv' => 8);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(41, count($styles));
+    }
+
+    public function testBeerStylePostIBU(){
+      $parameters = array('ibu' => 8);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(22, count($styles));
+    }
+
+    public function testBeerStylePostSRM(){
+      $parameters = array('srm' => 8);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(63, count($styles));
+    }
+
+    public function testBeerStylePostEBC(){
+      $parameters = array('ebc' => 8);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(53, count($styles));
+    }
+
+    public function testBeerStylePostYear(){
+      $parameters = array('year' => 2013);
+      $this->post('/api/beerstyles', $parameters);
+      $styles = json_decode($this->response->body());
+      $this->assertEquals(200, $this->response->status());
+      $this->assertSame(127, count($styles));
     }
 }
