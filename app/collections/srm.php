3269adc7fbf50ee155ff660966d69c8ad6156a0e
griffithben <griffith.ben@gmail.com>
2014-04-07 00:48:40 -0500

    forgot srm

--- app/collections/srm.php
+++ app/collections/srm.php
@@ -8,12 +8,4 @@ use App\Collections\AbstractCollection;
 class SrmCollection extends AbstractCollection {
   protected $_table = "srms";
   protected $_pk = "id";
-
-  public function fields($post){
-
-    if(!is_object($post))
-      return array();
-
-    $select = $this->select();
-  }
 }
