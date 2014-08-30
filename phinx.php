4b0832290dc3d62359c0891d40664b423588208f
griffithben <griffith.ben@gmail.com>
2014-08-29 23:43:46 -0500

    phinx remote php file for migrations

---
+++ phinx.php
@@ -0,0 +1,22 @@
+<?php
+  if(null == getenv("ENVIRONMENT")){
+    Dotenv::load(__DIR__);
+  }
+  $mysql = parse_url(getenv(getenv("ENVIRONMENT")."_DATABASE_URL"));
+  return array(
+    "paths" => array(
+        "migrations" => "migrations"
+    ),
+    "environments" => array(
+      "default_migration_table" => "_migrations",
+      "default_database" => "prod",
+      "prod" => array(
+        "adapter" => "mysql",
+        "host" => $mysql["host"],
+        "name" => substr($mysql["path"],1),
+        "user" => $mysql["user"],
+        "pass" => $mysql["pass"],
+        "port" => 3306
+      )
+    )
+  );
